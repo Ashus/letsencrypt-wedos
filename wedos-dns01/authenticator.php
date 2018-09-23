77ab5f6f03e52bd59c710e9ff6d0b5e3efc9e2d8
tona_kriz <tonakriz@gmail.com>
2018-09-23 20:23:58 +0200

    Better waiting mechanism to check for DNS updates

--- wedos-dns01/authenticator.php
+++ wedos-dns01/authenticator.php
@@ -17,4 +17,32 @@ $r = new \wedosDns01\dnsDomainCommitRequest();
 $r->name = CERTBOT_DOMAIN;
 wedosDns()->dnsDomainCommit($r);
 
-sleep(500);
+// Loop until updated DNS is online or for 15 minutes
+$time = time();
+while (true) {
+	// Get DNS record
+	$dns = dns_get_record('_acme-challenge' + CERTBOT_DOMAIN, DNS_TXT);
+  
+	if (sizeof($dns) > 0) {
+		$ok = false;
+		
+		// Check if any DNS record is 
+		foreach ($dns as $rec) {
+			if ($rec['txt'] === CERTBOT_VALIDATION) {
+				$ok = true;
+				break;
+			}		
+		}
+		
+		if ($ok) break;
+	}
+
+	// Wait 10s before next request
+	sleep(10);
+		
+	// Don't stay in the loop for longer than 15 minutes
+	if ((time() - $time) >= (15 * 60)) break;
+}
+
+// Give some extra time to the DNS to make sure its updated everywhere
+sleep(30);
